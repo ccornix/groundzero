4d30bb2823e54978eb798b2f441cf5cbcce226fd
ccornix <ccornix1758@gmail.com>
2024-03-20 23:48:54 +0100

    refactor: devilutionx package definition

--- packages/devilutionx.nix
+++ packages/devilutionx.nix
@@ -1,19 +1,19 @@
-{ pkgs, ... }:
+{ appimageTools, devilutionx, fetchurl, stdenv, ... }:
 
 let
-  inherit (pkgs.stdenv.hostPlatform) system;
+  inherit (stdenv.hostPlatform) system;
 in
 if system == "x86_64-linux" then
   let
     name = "devilutionx";
     version = "1.5.2";
   in
-  pkgs.appimageTools.wrapType2 {
+  appimageTools.wrapType2 {
     inherit name version;
-    src = pkgs.fetchurl {
+    src = fetchurl {
       url = "https://github.com/diasurgical/devilutionX/releases/download/${version}/devilutionx-linux-x86_64.appimage";
       sha256 = "sha256-cX5X3URheW+exdrH/TPvRRW71B3EGJ63j0z0LitHoEg=";
     };
   }
 else
-  pkgs.devilutionx # Fallback
+  devilutionx # Fallback
