857e18a348887fc2e23f33049f1145f58a296522
ccornix <ccornix1758@gmail.com>
2024-03-25 08:56:25 +0100

    fix(ccornix@thyme): set my.primaryDisplayResolution

--- home/configs/ccornix_at_thyme.nix
+++ home/configs/ccornix_at_thyme.nix
@@ -4,6 +4,8 @@
   imports = [ inputs.self.homeModules.default ./ccornix.nix ];
 
   my = {
+    primaryDisplayResolution = { horizontal = 1366; vertical = 768; };
+
     desktop.enable = true;
     gaming = {
       devilutionx.enable = true;
