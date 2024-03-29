49420a2c152d6984b20fa811426be0c25d2dbf28
ccornix <ccornix1758@gmail.com>
2024-03-29 16:35:54 +0100

    feat(flake.nix): re-enable nixpkgs-unstable input

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     impermanence.url = "github:nix-community/impermanence";
     disko = {
