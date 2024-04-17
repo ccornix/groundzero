b80b3a71fb34503b030f9ad54ea992f01b2b0f9f
ccornix <ccornix1758@gmail.com>
2024-04-17 09:47:27 +0200

    fix(ccornix@debian): nix package and PATH

--- home/configs/ccornix_at_debian.nix
+++ home/configs/ccornix_at_debian.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
   imports = [ inputs.self.homeModules.default ./ccornix.nix ];
@@ -7,11 +7,12 @@
     desktop.enable = false;
   };
 
-  home.stateVersion = "23.11";
+  home = {
+    packages = [ config.nix.package ];
+    sessionPath = [ "$HOME/.nix-profile/bin" ];
+    stateVersion = "23.11";
+  };
 
-  # Explicitly specify Nix package so that the HM activation script would pick
-  # that up
-  # https://github.com/nix-community/home-manager/blob/fa8c16e2452bf092ac76f09ee1fb1e9f7d0796e7/modules/home-environment.nix#L695
   nix = {
     enable = true;
     package = pkgs.nixVersions.nix_2_19;
