a8cf0c01f7315c61845c3607baf9d18371cab0d1
ccornix <ccornix1758@gmail.com>
2024-04-17 08:52:09 +0200

    fix(ccornix@debian): add Nix with explicit package specification

--- home/configs/ccornix_at_debian.nix
+++ home/configs/ccornix_at_debian.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [ inputs.self.homeModules.default ./ccornix.nix ];
@@ -8,4 +8,12 @@
   };
 
   home.stateVersion = "23.11";
+
+  # Explicitly specify Nix package so that the HM activation script would pick
+  # that up
+  # https://github.com/nix-community/home-manager/blob/fa8c16e2452bf092ac76f09ee1fb1e9f7d0796e7/modules/home-environment.nix#L695
+  nix = {
+    enable = true;
+    package = pkgs.nixVersions.nix_2_19;
+  };
 }
