a1aa836faa9a19915180f48c0f7c4529726d3fda
ccornix <ccornix1758@gmail.com>
2024-09-01 15:05:42 +0200

    feat(devilutionx): update to 1.5.3

--- packages/devilutionx.nix
+++ packages/devilutionx.nix
@@ -5,15 +5,17 @@ let
 in
 if system == "x86_64-linux" then
   let
-    name = "devilutionx";
-    version = "1.5.2";
+    version = "1.5.3";
   in
   appimageTools.wrapType2 {
-    inherit name version;
+    name = "devilutionx";
+    inherit version;
     src = fetchurl {
       url = "https://github.com/diasurgical/devilutionX/releases/download/${version}/devilutionx-linux-x86_64.appimage";
-      sha256 = "sha256-cX5X3URheW+exdrH/TPvRRW71B3EGJ63j0z0LitHoEg=";
+      sha256 = "sha256-Ta3s+sjMgG1Ebd626m+Ynw7+VXabNJhwmXko8+VE6LQ=";
     };
   }
 else
-  devilutionx # Fallback
+  builtins.trace
+    "No appimage for the current system, falling back to nixpkgs devilutionx"
+    devilutionx
