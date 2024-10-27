696449535323cb3ec4d0dda3d441c8dce058c137
ccornix <ccornix1758@gmail.com>
2024-10-27 05:11:58 +0100

    fix(ccornix): replace pubkey @ c236m by pubkey @ b560

--- nixos/configs/ccornix.nix
+++ nixos/configs/ccornix.nix
@@ -10,7 +10,7 @@ in
     hashedPasswordFile = "/persist/secrets/ccornix-password";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHPbbRvx53TLy1P9jnOR1PxmxMUHHsP/hfLuuZHE511G ccornix@b550"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOoC9C6YbE5HpGdFguS75MXnIy8XL+3Q/Tlxy26G2NE0 ccornix@c236m"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGh1n7HG8vW3W2h5RoqD5KEuWsEzMXz/nKwrPegUccXZ ccornix@b560"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEP1pxfp1gQ4nwlMkDR9hMQqRmD+G9p1lbzPzwLWDlHc ccornix@x13g2"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA9Kqnlg04ODbr1Je4HogTm+ry8KmesUNtGa+8x2H9fw ccornix@x230"
     ];
