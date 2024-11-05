d4b1fe4ee7a4e31fa8ee77ae7e3130074eafb560
ccornix <ccornix1758@gmail.com>
2024-11-05 08:43:22 +0100

    fix(ccornix@x13g2): kanshi config

--- home/configs/ccornix_at_x13g2.nix
+++ home/configs/ccornix_at_x13g2.nix
@@ -29,14 +29,14 @@
         outputs = [
           {
             criteria = "eDP-1";
-            position = "1920,0";
+            position = "0,0";
           }
           {
             # Use make-model-serial criterion for external monitors as the name
             # (DP-?) may change when reconnected. Get it using:
             # swaymsg -t get_outputs
             criteria = "Iiyama North America PL2493H 1211424213213";
-            position = "0,0";
+            position = "-1920,0";
           }
         ];
       };
