e95df425d438b6114fdce3f21fa8d6109d81ce65
ccornix <ccornix1758@gmail.com>
2025-01-19 16:51:23 +0100

    feat: update flake.nix to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Personal NixOS and Home Manager configurations.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     impermanence.url = "github:nix-community/impermanence";
     disko = {
@@ -10,7 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-colors.url = "github:misterio77/nix-colors";
