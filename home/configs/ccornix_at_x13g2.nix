bcda1812a94ab2fe84de9a20d7f93b8f42165385
ccornix <ccornix1758@gmail.com>
2025-02-07 08:45:30 +0100

    fix: adapt ccornix home configs to new monitor

--- home/configs/ccornix_at_x13g2.nix
+++ home/configs/ccornix_at_x13g2.nix
@@ -35,7 +35,7 @@
             # Use make-model-serial criterion for external monitors as the name
             # (DP-?) may change when reconnected. Get it using:
             # swaymsg -t get_outputs
-            criteria = "Iiyama North America PL2493H 1211424213213";
+            criteria = "ASUSTek COMPUTER INC PA248QV S4LMQS044196";
             position = "-1920,0";
           }
         ];
