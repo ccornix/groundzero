9ae4c09815ecd4cc035eaa349677cb28926fe903
ccornix <ccornix1758@gmail.com>
2025-05-18 12:28:17 +0200

    feat: reintroduce host c236m

---
+++ home/configs/ccornix_at_c236m.nix
@@ -0,0 +1,24 @@
+{ inputs, ... }:
+
+{
+  imports = [ inputs.self.homeModules.default ./ccornix.nix ];
+
+  my = {
+    desktop = {
+      enable = true;
+      latex.enable = true;
+    };
+    gaming = {
+      devilutionx.enable = true;
+      diablo2.enable = true;
+      wine.enable = true;
+    };
+    virtualization.enable = true;
+  };
+
+  # wayland.windowManager.sway.config.output = {
+  #   HDMI-A-1 = { adaptive_sync = "on"; };
+  # };
+
+  home.stateVersion = "24.11";
+}
---
+++ nixos/configs/c236m.nix
@@ -0,0 +1,216 @@
+# Custom Intel Skylake desktop PC (C236)
+
+{ inputs, pkgs, config, lib, ... }:
+
+{
+  imports = [
+    inputs.nixpkgs.nixosModules.notDetected
+    inputs.disko.nixosModules.disko
+    inputs.self.nixosModules.default
+    ./ccornix.nix
+  ];
+
+  my = {
+    desktop.enable = true;
+    network = {
+      shares.enable = true;
+      tailscale.enable = true;
+    };
+    virtualization = {
+      enable = true;
+      persistent = false;
+    };
+    zfs.enable = true;
+  };
+
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "ahci"
+        "nvme"
+        "sd_mod"
+        "usb_storage"
+        "usbhid"
+        "xhci_pci"
+      ];
+      kernelModules = [ "amdgpu" ];
+    };
+    kernelModules = [ "kvm-intel" ];
+    kernelParams = [ "mitigations=off" ];
+    loader.systemd-boot.enable = true;
+  };
+
+  # neededForBoot flag is not settable from disko
+  fileSystems = {
+    "/var/log".neededForBoot = true;
+    "/persist".neededForBoot = true;
+  };
+
+  swapDevices = [ ];
+
+  hardware = {
+    cpu.intel.updateMicrocode = lib.mkDefault
+      config.hardware.enableRedistributableFirmware;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = [ pkgs.amdvlk ];
+      extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+    };
+  };
+
+  networking.hostName = "c236m";
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+
+  system.stateVersion = "24.11";
+
+  disko.devices = {
+    disk = {
+      system = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b448b4e805a4b";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                # Fix world-accessible /boot/loader/random-seed
+                # https://github.com/nix-community/disko/issues/527#issuecomment-1924076948
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "rpool";
+              };
+            };
+          }; # partitions
+        }; # content
+      }; # system
+      # vm = {
+      #   type = "disk";
+      #   device = "/dev/disk/by-id/wwn-0x500a0751e67d29e6";
+      #   content = {
+      #     type = "gpt";
+      #     partitions = {
+      #       vm = {
+      #         size = "100%";
+      #         content = {
+      #           type = "filesystem";
+      #           format = "ext4";
+      #           mountpoint = "/var/lib/libvirt";
+      #         };
+      #       };
+      #     }; # partitions
+      #   }; # content
+      # }; # vm
+      # scratch = {
+      #   type = "disk";
+      #   device = "/dev/disk/by-id/wwn-0x500a0751e5ee2539";
+      #   content = {
+      #     type = "gpt";
+      #     partitions = {
+      #       scratch = {
+      #         size = "100%";
+      #         content = {
+      #           type = "filesystem";
+      #           format = "ext4";
+      #           mountpoint = "/scratch";
+      #         };
+      #       };
+      #     }; # partitions
+      #   }; # content
+      # }; # scratch
+    }; # disk
+    zpool = {
+      rpool = {
+        type = "zpool";
+        rootFsOptions = {
+          acltype = "posixacl";
+          dnodesize = "auto";
+          canmount = "off";
+          xattr = "sa";
+          relatime = "on";
+          normalization = "formD";
+          mountpoint = "none";
+          compression = "lz4";
+          "com.sun:auto-snapshot" = "false";
+        };
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+
+        datasets = {
+          local = {
+            type = "zfs_fs";
+            options.mountpoint = "none";
+          };
+          safe = {
+            type = "zfs_fs";
+            options.mountpoint = "none";
+          };
+          "local/reserved" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "none";
+              reservation = "5GiB";
+            };
+          };
+          "local/root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options.mountpoint = "legacy";
+            postCreateHook = ''
+              zfs snapshot rpool/local/root@blank
+            '';
+          };
+          "local/nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options = {
+              atime = "off";
+              canmount = "on";
+              mountpoint = "legacy";
+              "com.sun:auto-snapshot" = "true";
+            };
+          };
+          "local/log" = {
+            type = "zfs_fs";
+            mountpoint = "/var/log";
+            options = {
+              mountpoint = "legacy";
+              "com.sun:auto-snapshot" = "true";
+            };
+          };
+          "safe/home" = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            options = {
+              mountpoint = "legacy";
+              "com.sun:auto-snapshot" = "true";
+            };
+          };
+          "safe/persist" = {
+            type = "zfs_fs";
+            mountpoint = "/persist";
+            options = {
+              mountpoint = "legacy";
+              "com.sun:auto-snapshot" = "true";
+            };
+          };
+        }; # datasets
+      }; # rpool
+    }; # zpool
+  }; # disko.devices
+}
