ebf96b365ac25d4c47dbee6ed8a0b6252dbd6e3b
ccornix <ccornix1758@gmail.com>
2025-06-06 06:02:26 +0200

    chore: add todo for host removal

--- nixos/configs/default.nix
+++ nixos/configs/default.nix
@@ -1,18 +1,21 @@
-{ self, nixpkgs, ... } @ inputs:
+{ self, nixpkgs, ... }@inputs:
 
 let
   hosts = [
     "c236m"
+    "x13g2"
+    # TODO: remove these hosts
     # "b550"
     # "b560"
-    "x13g2"
   ];
 
-  mkNixosConfig = host: nixpkgs.lib.nameValuePair host (
-    nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [ ./${host}.nix ];
-    }
-  );
+  mkNixosConfig =
+    host:
+    nixpkgs.lib.nameValuePair host (
+      nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./${host}.nix ];
+      }
+    );
 in
 self.lib.mapListToAttrs mkNixosConfig hosts
