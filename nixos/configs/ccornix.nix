4891fac41b949e5b7424602c4ba3410f873e3f86
ccornix <ccornix1758@gmail.com>
2025-06-06 06:01:58 +0200

    feat: add new c236m public key

--- nixos/configs/ccornix.nix
+++ nixos/configs/ccornix.nix
@@ -1,4 +1,9 @@
-{ inputs, config, pkgs, ... }:
+{
+  inputs,
+  config,
+  pkgs,
+  ...
+}:
 
 let
   inherit (inputs) self;
@@ -9,23 +14,27 @@ in
     uid = 1000;
     hashedPasswordFile = "/persist/secrets/ccornix-password";
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA/WIfqmGcNHOQ4OXwNXAcmqwqO/TGrT6IHNkPHWSUoV ccornix@c236m"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEP1pxfp1gQ4nwlMkDR9hMQqRmD+G9p1lbzPzwLWDlHc ccornix@x13g2"
+      # TODO: remove these keys
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHPbbRvx53TLy1P9jnOR1PxmxMUHHsP/hfLuuZHE511G ccornix@b550"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGh1n7HG8vW3W2h5RoqD5KEuWsEzMXz/nKwrPegUccXZ ccornix@b560"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEP1pxfp1gQ4nwlMkDR9hMQqRmD+G9p1lbzPzwLWDlHc ccornix@x13g2"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA9Kqnlg04ODbr1Je4HogTm+ry8KmesUNtGa+8x2H9fw ccornix@x230"
     ];
     shell = pkgs.bashInteractive;
-    extraGroups = [
-      "wheel"
-      "audio"
-      "video"
-      "input"
-      "dialout" # to access serial ports
-    ] ++ self.lib.filterExistingGroups config [
-      "networkmanager"
-      "scanner"
-      "lp"
-      "libvirtd"
-    ];
+    extraGroups =
+      [
+        "wheel"
+        "audio"
+        "video"
+        "input"
+        "dialout" # to access serial ports
+      ]
+      ++ self.lib.filterExistingGroups config [
+        "networkmanager"
+        "scanner"
+        "lp"
+        "libvirtd"
+      ];
   };
 }
