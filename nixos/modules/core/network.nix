ec615e22507d6024050d5235b24d002ee2522cab
ccornix <ccornix1758@gmail.com>
2025-09-14 09:02:56 +0200

    fix(firewall): allow iperf3 TCP port

--- nixos/modules/core/network.nix
+++ nixos/modules/core/network.nix
@@ -7,7 +7,12 @@
       enable = true;
       wifi.scanRandMacAddress = false;
     };
-    firewall.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        5201  # iperf3
+      ];
+    };
 
     # Generate the hostId from the hostname
     hostId = builtins.substring 0 8 (
