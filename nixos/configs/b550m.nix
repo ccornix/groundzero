5574b56e739b86928ed061c0b47fa0d587aa5c07
ccornix <ccornix1758@gmail.com>
2025-09-17 08:37:39 +0200

    fix: remove AMDVLK package references in favor of RADV

--- nixos/configs/b550m.nix
+++ nixos/configs/b550m.nix
@@ -70,8 +70,6 @@
     graphics = {
       enable = true;
       enable32Bit = true;
-      extraPackages = [ pkgs.amdvlk ];
-      extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
     };
   };
 
--- nixos/configs/x13g2.nix
+++ nixos/configs/x13g2.nix
@@ -71,8 +71,6 @@ in
     graphics = {
       enable = true;
       enable32Bit = true;
-      extraPackages = [ pkgs.amdvlk ];
-      extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
     };
   };
 
